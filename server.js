c2a1a3ce3e54a6b6060abff2310d05d7c16ba5f2
an0nh4x0r <swappp7singh@gmail.com>
2016-08-26 13:30:28 +0530

    request function ready, it has url as parameter and a callback function having error status, response, html parameters.

--- server.js
+++ server.js
@@ -7,7 +7,35 @@ var app = express();
 
 app.get('/scrape', function(req, res){
 
-	// All the web scraping magic will happen here
+	// First the url from where we'll scrape the data
+	url = 'www.imdb.com/title/tt1663202/';
+
+	// The structure of the request url comes here
+	// The first parameter is our url
+	// The callback function takes three parameter, an error, response status code, and the html
+
+	request(url, function(error, response, html){
+
+		// First we'll make sure no error occured while making the request
+
+		if (!error) {
+
+			// Now we'll use cheerio library which is the jQuery of the server
+
+			var $ = cheerio.load(html);
+
+			// Finally we'll define the variables we're going to capture
+
+			var title, release, rating;
+			var json = {
+				title = "",
+				release = "",
+				rating = ""
+			};
+
+		}
+
+	})
 
 })
 
